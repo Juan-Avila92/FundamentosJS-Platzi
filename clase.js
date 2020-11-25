e6b88cdbb6b4ba5590e7c48ecb78a6605dc0c9e1
Juan-Avila92 <juanavila1992@hotmail.com>
2020-11-24 21:31:55 -0500

    cantidad de letras en el nombre

--- clase.js
+++ clase.js
@@ -3,3 +3,6 @@ var apellido = 'Avila'
 console.log('Hola ' + nombre + ' ' + apellido)
 
 var nombreEnMayusculas = nombre.toUpperCase()
+var apellidoEnMinusculas = apellido.toLowerCase()
+var primeraLetraDelNombre = nombre.charAt(0)
+var cantidadDeLetrasDelNomber = nombre.length
