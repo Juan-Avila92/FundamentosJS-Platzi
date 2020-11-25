75a6e36c6a4adad8e8e17fa5a5e0fceae1dba6ba
Juan-Avila92 <juanavila1992@hotmail.com>
2020-11-24 21:35:59 -0500

    Forma para hacer interpolacion de texto en JS - Asi se concatena en JS

--- clase.js
+++ clase.js
@@ -6,3 +6,5 @@ var nombreEnMayusculas = nombre.toUpperCase()
 var apellidoEnMinusculas = apellido.toLowerCase()
 var primeraLetraDelNombre = nombre.charAt(0)
 var cantidadDeLetrasDelNomber = nombre.length
+
+var nombreCompleto = `${nombre} ${apellido}`
