92026f4b6d25e4100b077a026c1a9c9bcbc3611b
Juan-Avila92 <juanavila1992@hotmail.com>
2020-12-13 21:50:55 -0500

    onPeopleResponse - it gets the name of a starwrs person

--- claseCallbacks.js
+++ claseCallbacks.js
@@ -1,4 +1,11 @@
 const API_URL = 'https://swapi.dev/api/'
 const PEOPLE_URL = 'people/:id/'
 
-$.get(`${API_URL}${PEOPLE_URL.replace(':id', 1)}` )
+const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', 2)}`
+const opts = { crossDOmain: true }
+
+const onPeopleResponse = function (persona) {
+    console.log(`hola yo soy, ${persona.name}`)
+}
+
+$.get(lukeUrl, opts, onPeopleResponse)
